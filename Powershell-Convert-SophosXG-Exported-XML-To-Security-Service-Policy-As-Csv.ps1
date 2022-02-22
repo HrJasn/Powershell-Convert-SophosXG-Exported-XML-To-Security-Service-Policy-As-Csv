8835ddceb6172ecfcb4b32a37e6454c09f2ddaa1
HrJasn <hrjasn@gmail.com>
2022-02-22 10:08:01 +0800

    Add columns and fix output wrong data type.
    1. Add "Status" column.
    2. Add "SourceIPHosts" column.
    3. Change output type from array to json.

--- Powershell-Convert-SophosXG-Exported-XML-To-Security-Service-Policy-As-Csv.ps1
+++ Powershell-Convert-SophosXG-Exported-XML-To-Security-Service-Policy-As-Csv.ps1
@@ -11,6 +11,7 @@ New-Item -ItemType Directory -Force -Path "$($($CurrentPS1File.PSParentPath) + '
 $Interfaces = $XMLObj.Configuration.Interface
 $SecurityPolicys = $XMLObj.Configuration.SecurityPolicy
 $IPHosts = $XMLObj.Configuration.IPHost
+$IPHostGroups = $XMLObj.Configuration.IPHostGroup
 $Services = $XMLObj.Configuration.Services
 
 #Filting with mapped port.
@@ -28,6 +29,7 @@ for($SecurityPolicyIndex=0;$SecurityPolicyIndex -lt $SecurityPolicys.Count;$Secu
     if(($SecurityPolicys[$SecurityPolicyIndex].NonHTTPBasedPolicy.MappedPort -like $TargetTCPPort) -or (@($MatchedService).Count -gt 0)){
 
         $Name = $SecurityPolicys[$SecurityPolicyIndex].Name
+        $Status = $SecurityPolicys[$SecurityPolicyIndex].Status
         $Description = $SecurityPolicys[$SecurityPolicyIndex].Description
         $Zone = $SecurityPolicys[$SecurityPolicyIndex].NonHTTPBasedPolicy.SourceZones.Zone
         $HostedAddress = $SecurityPolicys[$SecurityPolicyIndex].NonHTTPBasedPolicy.HostedAddress
@@ -36,28 +38,58 @@ for($SecurityPolicyIndex=0;$SecurityPolicyIndex -lt $SecurityPolicys.Count;$Secu
         $ProtectedServer = $SecurityPolicys[$SecurityPolicyIndex].NonHTTPBasedPolicy.ProtectedServer
         $ServiceName = $MatchedService.Name
         $DestinationPort = $MatchedService.ServiceDetails.ServiceDetail.DestinationPort
-        $DestinationPort = $DestinationPort | ConvertTo-Json -Depth 100 | ConvertFrom-Json
 
-        forEach($IPHost in $IPHosts){
-            switch ($IPHost.HostType){
-                'IP' {if($IPHost.Name -eq $ProtectedServer){$HostIP = $IPHost.IPAddress}}
-                'IPRange' {}
-                'IPList' {}
-                'Network' {}
-                'System Host' {
-                    if($IPHost.Name -eq $HostedAddress){
-                        $InterFaceName = $HostedAddress -replace '#([^#:]+)[:]?[^:]*','$1'
-                        $PublicIP = ($Interfaces | ?{$_.Name -eq $InterFaceName}).IPAddress
+        $SourceNetworks = $SecurityPolicys[$SecurityPolicyIndex].SourceNetworks.Network
+        $SourceIPHosts = @()
+
+        forEach($SourceNetwork in $SourceNetworks){
+            forEach($IPHostGroup in $IPHostGroups){
+                if($SourceNetwork -eq $IPHostGroup.Name){
+                    forEach($IPHost in $IPHosts){
+                        forEach($IPGroupHost in $IPHostGroup.HostList.Host){
+                            if($IPGroupHost -eq $IPHost.Name){
+                                $SourceIPHosts += $IPHost.IPAddress
+                            }
+                        }
+                    }
+                }
+            }
+            forEach($IPHost in $IPHosts){
+                switch ($IPHost.HostType){
+                    'IP' {
+                        if($IPHost.Name -eq $ProtectedServer){$HostIP = $IPHost.IPAddress}
+                        if($SourceNetwork -eq $IPHost.Name){
+                            $SourceIPHosts += $IPHost.IPAddress
+                        }
+                    }
+                    'IPRange' {
+                        if($SourceNetwork -eq $IPHost.Name){
+                            $SourceIPHosts += "$($IPHost.StartIPAddress + '-' + $IPHost.EndIPAddress)"
+                        }
+                    }
+                    'IPList' {}
+                    'Network' {}
+                    'System Host' {
+                        if($IPHost.Name -eq $HostedAddress){
+                            $InterFaceName = $HostedAddress -replace '#([^#:]+)[:]?[^:]*','$1'
+                            $PublicIP = ($Interfaces | ?{$_.Name -eq $InterFaceName}).IPAddress
+                        }
                     }
                 }
             }
         }
 
+        $Zone = $Zone | ConvertTo-Json -Depth 100
+        $SourceIPHosts = $SourceIPHosts | ConvertTo-Json -Depth 100
+        $DestinationPort = $DestinationPort | ConvertTo-Json -Depth 100
+
         $Properties = [ordered]@{
             Index = $SecurityPolicyIndex;
             Name = $Name;
+            Status = $Status;
             Description = $Description;
             Zone = $Zone;HostedAddress = $HostedAddress;
+            SourceIPHosts = $SourceIPHosts;
             PublicIP = $PublicIP;
             MappedPort = $MappedPort;
             ProtectedZone = $ProtectedZone;
